bfb5e9cc2e2b7af0efb6592610ed51621a95efa5
Jerald Tonmoy Dias <tonmoyjareld@gmail.com>
2022-05-02 15:48:43 +0600

    frontend  file setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,15 @@
 import React from "react";
 
+import { container, AppBar, Typegraphy, Grow, Grid } from "@material-ui/core";
 export default function App() {
-  return <div>App</div>;
+  return (
+    <Container>
+      <AppBar position="static" color="inherit">
+        <Typegraphy varient="h2" align="center">
+          TravelMemories
+        </Typegraphy>
+        <img src={memories} alt="memories" height="60" />
+      </AppBar>
+    </Container>
+  );
 }
